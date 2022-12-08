dcc8655c577e0be932feff320d1b9d73ce34012c
Matthew Cen <6173495+matthewcen@users.noreply.github.com>
2022-12-07 22:38:47 -0500

    replace deprecated tornardo AsyncTestCase with native unittest.IsolatedAsyncioTestCase

--- wpull/testing/async_.py
+++ wpull/testing/async_.py
@@ -6,17 +6,8 @@ from tornado.testing import gen_test
 import asyncio
 
 
-# TODO: Replace class with Tornado's AsyncTestCase
-# http://stackoverflow.com/q/23033939/1524507
-class AsyncTestCase(unittest.TestCase):
-    def setUp(self):
-        self.event_loop = asyncio.new_event_loop()
-        self.event_loop.set_debug(True)
-        asyncio.set_event_loop(self.event_loop)
+class AsyncTestCase(unittest.IsolatedAsyncioTestCase()):
 
-    def tearDown(self):
-        self.event_loop.stop()
-        self.event_loop.close()
 
 def async_test(func=None, timeout=30):
     # gen_test uses environment variable ASYNC_TEST_TIMEOUT as default if set, otherwise 5 seconds
